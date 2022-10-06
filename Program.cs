528eb8d546993a107303fa409d60844fbfd88ede
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-10-06 18:33:21 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas1
+{
+    class Program
+    {
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+    }
+}
